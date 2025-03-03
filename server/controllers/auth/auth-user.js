363e30604dbeab689989586899dc5252aa902e39
rseangueco <rngueco2@up.edu.ph>
2025-03-03 08:50:42 +0800

    chore: updated gitignore

--- server/controllers/auth/auth-user.js
+++ server/controllers/auth/auth-user.js
@@ -1,5 +1,5 @@
 /*
-accept username and encrypted password
+accept username and encrypted password in req.body
 if they match, create json web token and return token with status 200
 else return error with status 401
 */
--- server/controllers/auth/validate-token.js
+++ server/controllers/auth/validate-token.js
@@ -1,4 +1,8 @@
 /*
-accept token and validate
-return status
+accept token in req.header
+validate with jwt
+if valid, return with status 200
+else return error with status 401
+
+note: keys should be in .env file
 */
