16a43e7a2474f731ab25933f762f02e3ad87084a
ella <eegarcia4@up.edu.ph>
2025-03-13 03:06:32 +0800

    feat: add initial routes for user auth (login and register)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import alumniRoutes from "./src/routes/alumniRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // routes
 app.use("/alumni", alumniRoutes);
+app.use("/auth", authRoutes);
 
 //export for testing
 export default app;
