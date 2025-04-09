610d744740650c4963101b2afce6067ee46ad31d
rseangueco <rngueco2@up.edu.ph>
2025-04-09 19:01:23 +0800

    fix: fixed undetected merge errors

--- server/src/routes/alumniRoutes.js
+++ server/src/routes/alumniRoutes.js
@@ -1,23 +1,22 @@
 import { Router } from 'express';
 const router = Router();
-import { createAlumni, getAllAlumni } from '../controllers/alumniController.js';
+import { alumniController } from '../controllers/modelControllers/alumniController.js';
+import { getAllAlumni } from '../controllers/modelControllers/alumniController.js';
 import { authenticateToken, authorizeRoles } from '../middleware/authMiddleware.js';
 
+// fetch all alumni profiles
+router.get('/read', alumniController.read);
+
 // create a new alumni profile (Admin only)
-router.post('/alumni', authenticateToken, authorizeRoles(['Admin']), createAlumni);
+// router.post('/alumni', authenticateToken, authorizeRoles(['Admin']), createAlumni);
 
 // fetch all alumni profiles (Admin and Alumni)
 router.get('/alumni', authenticateToken, authorizeRoles(['Admin', 'Alumni']), getAllAlumni);
 
-import { alumniController, getAllAlumni } from '../controllers/modelControllers/alumniController.js';
-
 // create a new alumni profile
 // router.post('/alumni', createAlumni);
 
-// read
-router.get('/read', alumniController.read);
-
-// fetch all alumni profiles
-router.get('/get', getAllAlumni);
+// // fetch all alumni profiles
+// router.get('/get', getAllAlumni);
 
 export default router;
