f65aa135d78489437894bd65fe7b6c7b49a33da6
AJTJ09 <atjavier1@up.edu.ph>
2025-04-14 17:26:02 +0800

    feat: added alumni route for RSVP creation

--- server/src/routes/alumniRoutes.js
+++ server/src/routes/alumniRoutes.js
@@ -3,6 +3,7 @@ const router = Router();
 import { alumniController } from '../controllers/modelControllers/alumniController.js';
 import { getAllAlumni } from '../controllers/modelControllers/alumniController.js';
 import { authenticateToken, authorizeRoles } from '../middleware/authMiddleware.js';
+import { createRSVP } from '../controllers/RSVPController/rsvpController.js';
 
 // fetch all alumni profiles
 router.get('/read', alumniController.read);
@@ -12,6 +13,12 @@ router.get('/read', alumniController.read);
 
 // fetch all alumni profiles (Admin and Alumni)
 router.get('/alumni', authenticateToken, authorizeRoles(['Admin', 'Alumni']), getAllAlumni);
+
+// Create RSVP (Alumni confirming their attendance)
+router.post('alumni/rsvp', authenticateToken, authorizeRoles(['Alumni']), createRSVP);
+
+// fetch all attendees for the event (Admin and Alumni)
+// router.get('alumni/event/:eventId/attendees', authenticateToken, authorizeRoles(['Alumin', 'Admin']), getEventAttendees);
 
 // create a new alumni profile
 // router.post('/alumni', createAlumni);
