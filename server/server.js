02b7e96d97ac7f6d7333c889a25d50194b9f50e9
koolano <koolano@up.edu.ph>
2025-04-14 14:44:58 +0800

    feat: Added file middleware and route for upload and downloading of files.

--- server/server.js
+++ server/server.js
@@ -2,18 +2,22 @@ import express from "express";
 import cors from "cors";
 import alumniRoutes from "./src/routes/alumniRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
+import fileRoutes from './src/routes/fileRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 // routes
 app.use("/alumni", alumniRoutes);
 app.use("/auth", authRoutes);
+app.use("/file", fileRoutes);
 
 // connect to MongoDB - commented cause this is should be in index.js but not sure if you need this for something
 // console.log("MONGO_URI:", process.env.MONGO_URI); // add this line to debug
