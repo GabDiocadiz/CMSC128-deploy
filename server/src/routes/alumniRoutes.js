6b3fc2cb51acf9ed3539a1589790ad218a026e3a
rseangueco <rngueco2@up.edu.ph>
2025-04-19 22:46:39 +0800

    refactor: moved validate token to middleware, changed export to a function, separated auth middleware to individual files

--- server/src/routes/alumniRoutes.js
+++ server/src/routes/alumniRoutes.js
@@ -2,21 +2,14 @@ import { Router } from 'express';
 const router = Router();
 import { alumniController } from '../controllers/modelControllers/alumniController.js';
 import { getAllAlumni } from '../controllers/modelControllers/alumniController.js';
-import { authenticateToken, authorizeRoles } from '../middleware/authMiddleware.js';
+import { validateToken } from '../middleware/validate-token.js';
+import { authorizeRoles } from '../middleware/authorize-roles.js';
 
 // fetch all alumni profiles
 router.get('/read', alumniController.read);
 
-// create a new alumni profile (Admin only)
-// router.post('/alumni', authenticateToken, authorizeRoles(['Admin']), createAlumni);
-
 // fetch all alumni profiles (Admin and Alumni)
-router.get('/alumni', authenticateToken, authorizeRoles(['Admin', 'Alumni']), getAllAlumni);
+router.get('/alumni', validateToken, authorizeRoles(['Admin', 'Alumni']), getAllAlumni);
 
-// create a new alumni profile
-// router.post('/alumni', createAlumni);
-
-// // fetch all alumni profiles
-// router.get('/get', getAllAlumni);
 
 export default router;
