9f57ca5e93102df42ea942e147f761b7c048c4c2
rseangueco <rngueco2@up.edu.ph>
2025-04-19 23:24:07 +0800

    feat: added basic admin controller

--- server/src/routes/alumniRoutes.js
+++ server/src/routes/alumniRoutes.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-const router = Router();
+import express from 'express';
 import { alumniController } from '../controllers/modelControllers/alumniController.js';
 import { getAllAlumni } from '../controllers/modelControllers/alumniController.js';
 import { validateToken } from '../middleware/validate-token.js';
 import { authorizeRoles } from '../middleware/authorize-roles.js';
+const router = express.Router();
 
-// fetch all alumni profiles
-router.get('/read', alumniController.read);
+// delete by email
+router.delete('/email/:email', alumniController.deleteByEmail);
 
 // fetch all alumni profiles (Admin and Alumni)
 router.get('/alumni', validateToken, authorizeRoles(['Admin', 'Alumni']), getAllAlumni);
