cd821a671789b68b8110cf2f13a10b44248ca864
AJTJ09 <atjavier1@up.edu.ph>
2025-04-25 04:49:42 +0800

    feat: added rsvp view and edit

--- server/src/controllers/RSVPController/rsvpController.js
+++ server/src/controllers/RSVPController/rsvpController.js
@@ -10,7 +10,7 @@ export const createRSVP = async (req, res) => {
 
         // check for any existing RSVPs of alumnus for chosen event
         if (existingRSVP) {
-            return res.status(400).json({ message: "You've already RSVP'd for this event." })
+            return res.status(400).json({ message: "You've already RSVP'd for this event." });
         }
 
         const newRSVP = await RSVP.create({ event, alumni: alumniID, status });
@@ -22,3 +22,47 @@ export const createRSVP = async (req, res) => {
     }
 }
 
+export const editRSVP = async (req, res) => {
+    try {
+        const { status } = req.body;
+        const { eventID } = req.params;
+        const alumniID = req.user._id;
+
+        const updatedRSVP = await RSVP.findOneAndUpdate(
+            { event: eventID, alumni: alumniID },
+            { $set: { status } },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedRSVP) {
+            return res.status(404).json({ message: "RSVP for event does not exist" });
+        }
+
+        res.status(200).json({ message: "RSVP updated successfully", updatedRSVP });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+};
+
+export const viewRSVP = async (req, res) => {
+    try {
+        const alumniID = req.user._id;
+
+        const existingRSVP = await RSVP.find({ alumni: alumniID })
+            .populate({
+                path: "event",
+                select: "event_name event_date venue"
+            })
+            .sort({ "event.event_date": -1 })
+            .lean();
+
+        if (existingRSVP.length === 0) {
+            return res.status(404).json({ message: "No existing RSVPs" });
+        }
+
+        res.status(200).json({ message: "RSVPs retrieved successfully", existingRSVP });
+    } catch (e) {
+        console.error("Error fetching RSVPs:", e.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
