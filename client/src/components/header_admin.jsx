ce20487e6d1e54560b6314c9edc85f54ec3560a8
Precious Ramil <pdramil@up.edu.ph>
2025-04-28 23:52:40 +0800

    feat: add profile dropdown menu

--- client/src/components/header_admin.jsx
+++ client/src/components/header_admin.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import speakerIcon from '../assets/Speaker_Icon.svg';
 export default function Navbar_admin() {
   const [isOpen, setIsOpen] = useState(false);
+  const [profileMenuOpen, setProfileMenuOpen] = useState(false);
   const handleSend=(e)=>{
     // await send= NULL;
     const send=0; //temporary var
@@ -64,9 +66,32 @@ export default function Navbar_admin() {
             </a>
 
             {/* Profile Icon inside Circle */}
-            <a href="/" className="w-10 h-10 bg-none text-white flex items-center justify-center rounded-full">
+            <div className="relative">
+              <div
+                onClick={() => setProfileMenuOpen(!profileMenuOpen)}
+                className="w-10 h-10 bg-none flex items-center justify-center rounded-full cursor-pointer"
+              >
                 <img src="src/assets/Human Icon.png" className="w-10 h-10" alt="Profile" />
-            </a>
+              </div>
+
+              {profileMenuOpen && (
+                <div className="absolute right-0 mt-1 bg-white !shadow-lg rounded-sm w-35 z-50 text-center text-sm border border-gray-400">
+                  <Link
+                    to={`/`}
+                    className="block w-full px-4 py-2 !text-gray-700 hover:bg-blue-100 focus:!outline-none"
+                    onClick={() => setProfileMenuOpen(false)}
+                  >
+                    View Profile
+                  </Link>
+                  <button
+                    // onClick={}
+                    className="block w-full px-4 py-2 !text-gray-700 hover:bg-[#891839] hover:!text-white focus:!outline-none"
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </div>
         </div>
       </div>
     </nav>
