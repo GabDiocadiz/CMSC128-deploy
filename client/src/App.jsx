3bb49990cead2285720ce3b826ff89d722d7ec28
rseangueco <rngueco2@up.edu.ph>
2025-04-28 22:44:12 +0800

    feat: accessing unauthorized pages no redirects to login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import { Results_page_accounts} from './components/sections/Results_accounts'
 import { Results_page_jobs } from './components/sections/Results_job'
 import { Results_page_events } from './components/sections/Results_event'
 import { AuthProvider } from './AuthContext'
+import { RoleRoute } from './ProtectedRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,13 +30,19 @@ function App() {
             <Route path="/" element={<Landing_page/>} /> 
             <Route path="/reg" element={<Registration/>} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/admin_main/:user_id" element={<Admin_main/>} />
-            <Route path="/home/:user_id" element={<MainPage/>} />
-            <Route path="/jobs/:user_id" element={<Results_page_jobs/>} />
-            <Route path="/job-details/:id/:user_id" element={<ViewJobDetails/>} />
-            <Route path="/events/:user_id" element={<Results_page_events/>} />
-            <Route path="/event-details/:id/:user_id" element={<ViewEventDetails/>} />
-            <Route path="/search-alumni/:id/:user_id" element={<Results_page_accounts/>} />
+
+            <Route element={<RoleRoute allowedRoles={['Admin']}/>}>
+              <Route path="/admin_main/:user_id" element={<Admin_main/>} />
+            </Route>
+
+            <Route element={<RoleRoute allowedRoles={['Admin', 'Alumni']}/>}>
+              <Route path="/home/:user_id" element={<MainPage/>} />
+              <Route path="/jobs/:user_id" element={<Results_page_jobs/>} />
+              <Route path="/job-details/:id/:user_id" element={<ViewJobDetails/>} />
+              <Route path="/events/:user_id" element={<Results_page_events/>} />
+              <Route path="/event-details/:id/:user_id" element={<ViewEventDetails/>} />
+              <Route path="/search-alumni/:id/:user_id" element={<Results_page_accounts/>} />
+            </Route>
 
           </Routes>
         </AuthProvider>
