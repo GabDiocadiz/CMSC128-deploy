2b845aa801a8b0c398aeb9e48b3e771280a3f23b
AJTJ09 <atjavier1@up.edu.ph>
2025-04-28 17:01:44 +0800

    feat: added event routes

--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@ import cookieParser from "cookie-parser";
 import alumniRoutes from "./src/routes/alumniRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import fileRoutes from './src/routes/fileRoutes.js';
+import eventRoutes from './src/routes/eventRoutes.js'
 import notificationRoutes from './src/routes/notificationRoutes.js';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 
-dotenv.config(); 
+dotenv.config();
 
 const VITE_API_URL = process.env.VITE_API_URL || 5173;
 
@@ -26,8 +27,8 @@ const __dirname = path.dirname(__filename);
 
 // middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,10 +39,12 @@ app.use("/alumni", alumniRoutes);
 app.use("/auth", authRoutes);
 app.use("/file", fileRoutes);
 app.use('/api/notifications', notificationRoutes);
+app.use("/events", eventRoutes);
+
 
 // temporary default route -- remove when connecting to frontend
 app.get('/', (req, res) => {
-    res.send('API is running');
+  res.send('API is running');
 });
 
 //export for testing
