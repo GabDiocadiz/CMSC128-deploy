81f609feec3ceb1b4e2c6a0e47700c9d53eab829
rseangueco <rngueco2@up.edu.ph>
2025-04-28 19:47:10 +0800

    fix: fixed event imports

--- server/src/controllers/modelControllers/eventController.js
+++ server/src/controllers/modelControllers/eventController.js
@@ -1,28 +1,38 @@
 import { Event } from '../../models/Event.js';
 import { createCRUDController } from '../middlewareControllers/createCRUDController/index.js';
 
-const eventController = createCRUDController(Event);
+export const eventController = {
+    ...createCRUDController(Event),
 
-eventController.read = async function (req, res) {
-    try {
-
-        const { sortBy } = req.query;
-        let events;
-
-        if (sortBy === "date") {
-            events = await Event.find().sort({ event_date: -1 }); // Sort newest first
-        } else if (sortBy === "title") {
-            events = await Event.find().sort({ event_name: 1 }); // Sort A → Z
-        } else {
-            events = await Event.find(); // Default: return unsorted events
+    async adminPageEvents(req, res) {
+        try {
+            const items = await Event.find()
+                .select('event_name event_date');
+            res.status(200).json(items);
+        } catch (err) {
+            res.status(400).json({ error: err.message})
         }
+    },
 
-        console.log("Fetched Events:", events.length);
-        res.status(200).json(events);
-    } catch (e) {
-        console.error("Error in eventController.read:", e);
-        res.status(500).json({ message: e.message });
+    async readSortByDate(req, res) {
+        try {
+
+            const { sortBy } = req.query;
+            let events;
+    
+            if (sortBy === "date") {
+                events = await Event.find().sort({ event_date: -1 }); // Sort newest first
+            } else if (sortBy === "title") {
+                events = await Event.find().sort({ event_name: 1 }); // Sort A → Z
+            } else {
+                events = await Event.find(); // Default: return unsorted events
+            }
+    
+            console.log("Fetched Events:", events.length);
+            res.status(200).json(events);
+        } catch (e) {
+            console.error("Error in eventController.read:", e);
+            res.status(500).json({ message: e.message });
+        }
     }
-};
-
-export default eventController;
+}
