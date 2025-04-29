6be6a3351457561692534d9a5957719056099ef5
Richhqw <rbpore@up.edu.ph>
2025-04-29 23:23:41 +0800

    Added Job Post and Event creation, added speed dial in admin to access create event

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,8 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import { Admin_main } from './components/sections/Admin_main'
 import { Results_page_accounts} from './components/sections/Results_accounts'
 import { Results_page_jobs } from './components/sections/Results_job'
-
+import { Create_Event } from './components/sections/Create_event'
+import { Post_Job } from './components/sections/post_job'
 function App() {
   const [count, setCount] = useState(0)
   
@@ -22,18 +23,18 @@ function App() {
       {
       <BrowserRouter>
         <Routes>
-
-          <Route path="/" element={<Landing_page/>} />  
+          
+          <Route path="/" element={<Admin_main/>} />  
           <Route path="/reg" element={<Registration/>} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/admin_main/:user_id" element={<Admin_main/>} />
+          <Route path="/admin_main" element={<Admin_main/>} />
           <Route path="/home/:user_id" element={<MainPage/>} />
           <Route path="/jobs/:user_id" element={<Results_page_jobs/>} />
           <Route path="/job-details/:id/:user_id" element={<ViewJobDetails/>} />
           <Route path="/event-details/:id/:user_id" element={<ViewEventDetails/>} />
-          <Route path="/search-alumni/:id/:user_id" element={<Results_page_accounts/>} />
-      
-          
+          <Route path="/search-alumni/:id/:user_id  " element={<Results_page_accounts/>} />
+          <Route path="/create_event" element={<Create_Event/>} />
+          <Route path="/post_job" element={<Post_Job/>} />
         </Routes>
         
       </BrowserRouter>
