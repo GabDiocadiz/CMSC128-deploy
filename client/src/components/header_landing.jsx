ad7cfe8043b9709c1356ac98711a4b52fb035d79
rseangueco <rngueco2@up.edu.ph>
2025-05-01 00:46:11 +0800

    fix: profile navigation

--- client/src/components/header_landing.jsx
+++ client/src/components/header_landing.jsx
@@ -4,7 +4,6 @@ export default function Navbar_landing() {
   return (
     
     <div>
-      {/* <Notification></Notification> */}
       <nav className="bg-white w-full py-1 fixed top-0 left-0">
         {/* Flexbox for proper alignment */}
         <div className="container flex justify-between items-center py-1 px-4">
