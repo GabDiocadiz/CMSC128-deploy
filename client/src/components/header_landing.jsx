c5e6f72a7a755e0f6d780b62275c02f77da41f8d
rseangueco <rngueco2@up.edu.ph>
2025-05-01 00:40:56 +0800

    fix: removed permanent notification modal in landing page

--- client/src/components/header_landing.jsx
+++ client/src/components/header_landing.jsx
@@ -4,7 +4,7 @@ export default function Navbar_landing() {
   return (
     
     <div>
-      <Notification></Notification>
+      {/* <Notification></Notification> */}
       <nav className="bg-white w-full py-1 fixed top-0 left-0">
         {/* Flexbox for proper alignment */}
         <div className="container flex justify-between items-center py-1 px-4">
--- client/src/components/sections/Landing_page.jsx
+++ client/src/components/sections/Landing_page.jsx
@@ -9,7 +9,6 @@ export const Landing_page = () => {
     <>
       <div className="w-scree h-auto">
         <Navbar_landing />
-        <notification />
       </div>
       <div className="bg-[url('src/assets/Building.png')] bg-cover bg-center w-full h-175 flex flex-col justify-between">
         <div className="flex justify-between items-start h-full text-white text-left pl-16 pt-24">
