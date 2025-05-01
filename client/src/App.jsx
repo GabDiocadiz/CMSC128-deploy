c9d3a42c4b66d899f66e9dc878900ca4d0e501bb
GabDiocadiz <gddiocadiz@up.edu.ph>
2025-05-01 09:19:38 +0800

    fix: /reg path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Landing_page/>} /> 
-            <Route path="/reg" element={<Results_page_jobs/>} />
+            <Route path="/reg" element={<Registration/>} />
             <Route path="/login" element={<Login/>} />
 
             <Route element={<RoleRoute allowedRoles={['Admin']}/>}>
