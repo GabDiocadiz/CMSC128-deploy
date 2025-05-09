e7c0af77a520c0aba9325dcb809e97fa440c2bd8
Precious Ramil <pdramil@up.edu.ph>
2025-05-09 12:57:22 +0800

    style: add bottom margin

--- client/src/components/sections/Results_event.jsx
+++ client/src/components/sections/Results_event.jsx
@@ -100,7 +100,7 @@ export const Results_page_events = ( ) => {
                         </div>
 
                         {/* Display events */}
-                        <div className="flex justify-center w-full overflow-x-hidden">
+                        <div className="flex justify-center w-full overflow-x-hidden mb-35">
                             {events.length > 0 ? (
                                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                                     {events.map(event => (
