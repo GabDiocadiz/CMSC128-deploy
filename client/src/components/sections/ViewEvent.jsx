49d8416c8a20d3088d9123b933758328c5a71cdb
AJTJ09 <atjavier1@up.edu.ph>
2025-05-15 16:29:07 +0800

    feat: create RSVP

--- client/src/components/sections/ViewEvent.jsx
+++ client/src/components/sections/ViewEvent.jsx
@@ -15,7 +15,8 @@ export default function ViewEventDetails() {
     const [isLoading, setIsLoading] = useState(true);
     const { authAxios, user } = useAuth();
     const navigate = useNavigate();
-    console.log(id);
+    // console.log(id);
+
 
     useEffect(() => {
         const fetchedEvent = async () => {
@@ -68,7 +69,24 @@ export default function ViewEventDetails() {
         }
         ScrollToTop();
     }, [id]);
-    const  handleRSVP=()=>{
+
+    const handleRSVP = () => {
+        const rsvpData = {
+            status: "Attending",
+        };
+
+        authAxios.post(`http://localhost:5050/events/create-rsvp/${id}`, rsvpData)
+            .then((response) => {
+                console.log("RSVP successful:", response.data);
+                alert("RSVP successful");
+                // Optionally, refresh event data to update attendees
+                // window.location.reload();
+            })
+            .catch((error) => {
+                console.error("Error RSVPing:", error);
+                alert("Error RSVPing");
+            });
+
 
     }
     if (!event) return <div>Event not found</div>;
@@ -81,72 +99,71 @@ export default function ViewEventDetails() {
 
             <div className="w-screen pt-12">
 
-            {isLoading ? (
-                <div className="min-w-screen min-h-screen bg-gray-200 flex justify-center items-center">
-                    <div className="w-16 h-16 border-4 border-[#145C44] border-t-transparent rounded-full animate-spin"></div>
-                </div>
-            ) : (
-                <div className="min-h-[85vh] bg-cover bg-center text-white flex justify-center items-center px-4 sm:px-8 md:px-16 pb-10 w-full"
-                    style={{
-                        backgroundImage: `url(${
-                        typeof event?.files?.[0] === 'string' && event.files[0].trim()
-                            ? `http://localhost:5050/uploads/${event.files[0].trim()}`
-                            : default_eventbg
-                        })`
-                    }}
+                {isLoading ? (
+                    <div className="min-w-screen min-h-screen bg-gray-200 flex justify-center items-center">
+                        <div className="w-16 h-16 border-4 border-[#145C44] border-t-transparent rounded-full animate-spin"></div>
+                    </div>
+                ) : (
+                    <div className="min-h-[85vh] bg-cover bg-center text-white flex justify-center items-center px-4 sm:px-8 md:px-16 pb-10 w-full"
+                        style={{
+                            backgroundImage: `url(${typeof event?.files?.[0] === 'string' && event.files[0].trim()
+                                ? `http://localhost:5050/uploads/${event.files[0].trim()}`
+                                : default_eventbg
+                                })`
+                        }}
                     >
-                    <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
+                        <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
 
-                    <div className="relative z-10 flex flex-col lg:flex-row justify-center items-center w-full h-full">
-                        <div className="w-full lg:w-[40%]">
-                            <div
-                                className="flex items-center gap-2 cursor-pointer text-white hover:text-gray-200 mb-6 mt-10 sm:mt-[5vh] lg:mt-[8vh]"
-                                onClick={() => navigate(-1)}
-                            >
-                                <IoIosArrowBack className="text-sm" />
-                                <span className="text-sm font-light">Back</span>
-                            </div>
-                            <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl lg:text-left font-extrabold mb-4">
-                                {event.event_name}
-                            </h1>
-                            <p className="text-lg sm:text-2xl lg:text-3xl font-bold flex items-center text-left gap-3 mb-5">
-                                <FaLocationDot className="text-2xl sm:text-3xl lg:text-4xl" />
-                                {event.venue}
-                            </p>
-                            <p className="text-sm sm:text-base lg:text-lg text-left ml-2">
-                                Date:{" "}
-                                {new Date(event.event_date).toLocaleDateString("en-US", {
-                                    year: "numeric",
-                                    month: "long",
-                                    day: "numeric",
-                                })}
-                            </p>
-
-                            {true && (
-                            <div className="mt-10 flex justify-center lg:justify-start">
-                                <div className="grid grid-cols-2 gap-4 sm:gap-x-10">
-                                <Link to={`/donate/${event.id}`}>
-                                    <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-[#145C44] hover:ring-2 text-white font-bold h-[45px] sm:h-[55px] w-[180px] px-4 py-2 rounded-md">
-                                    Donate
-                                    </button>
-                                </Link>
-                                <Link onClick={handleRSVP}>
-                                    <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-[#891839] hover:ring-2 text-white font-bold h-[45px] sm:h-[55px] w-[200px] px-4 py-2 rounded-md">
-                                    Will be Attending
-                                    </button>
-                                </Link>
+                        <div className="relative z-10 flex flex-col lg:flex-row justify-center items-center w-full h-full">
+                            <div className="w-full lg:w-[40%]">
+                                <div
+                                    className="flex items-center gap-2 cursor-pointer text-white hover:text-gray-200 mb-6 mt-10 sm:mt-[5vh] lg:mt-[8vh]"
+                                    onClick={() => navigate(-1)}
+                                >
+                                    <IoIosArrowBack className="text-sm" />
+                                    <span className="text-sm font-light">Back</span>
                                 </div>
-                            </div>
-                            )}
-                        </div>
+                                <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl lg:text-left font-extrabold mb-4">
+                                    {event.event_name}
+                                </h1>
+                                <p className="text-lg sm:text-2xl lg:text-3xl font-bold flex items-center text-left gap-3 mb-5">
+                                    <FaLocationDot className="text-2xl sm:text-3xl lg:text-4xl" />
+                                    {event.venue}
+                                </p>
+                                <p className="text-sm sm:text-base lg:text-lg text-left ml-2">
+                                    Date:{" "}
+                                    {new Date(event.event_date).toLocaleDateString("en-US", {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric",
+                                    })}
+                                </p>
 
-                        <div className="w-full lg:w-[50%] mt-8 mb-10 lg:mt-[10vh] lg:ml-[5vw] px-5 pt-7 ">
-                            <div className="text-left relative whitespace-pre-line border-2 rounded-2xl p-5 h-[20vh] sm:h-[20vh] md:h-[20vh] lg:h-[50vh] overflow-y-auto pr-5 bg-transparent text-white scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 dark:scrollbar-thumb-neutral-500 dark:scrollbar-track-neutral-700">
-                                {event.event_description}
+                                {true && (
+                                    <div className="mt-10 flex justify-center lg:justify-start">
+                                        <div className="grid grid-cols-2 gap-4 sm:gap-x-10">
+                                            <Link to={`/donate/${event.id}`}>
+                                                <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-[#145C44] hover:ring-2 text-white font-bold h-[45px] sm:h-[55px] w-[180px] px-4 py-2 rounded-md">
+                                                    Donate
+                                                </button>
+                                            </Link>
+                                            <Link onClick={handleRSVP}>
+                                                <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-[#891839] hover:ring-2 text-white font-bold h-[45px] sm:h-[55px] w-[200px] px-4 py-2 rounded-md">
+                                                    Will be Attending
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="w-full lg:w-[50%] mt-8 mb-10 lg:mt-[10vh] lg:ml-[5vw] px-5 pt-7 ">
+                                <div className="text-left relative whitespace-pre-line border-2 rounded-2xl p-5 h-[20vh] sm:h-[20vh] md:h-[20vh] lg:h-[50vh] overflow-y-auto pr-5 bg-transparent text-white scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 dark:scrollbar-thumb-neutral-500 dark:scrollbar-track-neutral-700">
+                                    {event.event_description}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 )}
             </div>
 
