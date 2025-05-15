2c14b51f44a3a39486b7d16c1ccfa7ed3e129fe6
AJTJ09 <atjavier1@up.edu.ph>
2025-05-15 21:45:50 +0800

    chore: added event field

--- server/src/models/Donation.js
+++ server/src/models/Donation.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const DonationSchema = new Schema({
+    event: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
     donor: { type: Schema.Types.ObjectId, ref: 'Alumni', required: true },
     amount: { type: Number, min: 0, required: true },
     donation_date: { type: Date, default: Date.now },
