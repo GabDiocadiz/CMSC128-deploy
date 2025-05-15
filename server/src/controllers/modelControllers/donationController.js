a6a64575d0820cc926968b898dc4fc444a335f2f
AJTJ09 <atjavier1@up.edu.ph>
2025-05-15 18:39:03 +0800

    feat: added donation Controller

---
+++ server/src/controllers/modelControllers/donationController.js
@@ -0,0 +1,6 @@
+import Donation from '../../models/Donation.js';
+import { createCRUDController } from '../middlewareControllers/createCRUDController/index.js';
+
+export const donationController = {
+    ...createCRUDController(Donation),
+}
